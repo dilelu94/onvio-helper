32275aba85f0e5bb3662eb97c2d1dbf9aaf6b86c
dilelu <diego.leonel.luque@gmail.com>
2026-03-27 21:22:49 -0300

    Fix: XLSX import in ExcelDB using createRequire for compatibility

--- src/services/ExcelDB.js
+++ src/services/ExcelDB.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
-import * as XLSX from 'xlsx';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const XLSX = require('xlsx');
 
 class ExcelDB {
   constructor() {
